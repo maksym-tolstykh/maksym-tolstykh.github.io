41cf2165ce709db63b7e9992d8f096bf5dea777c
maxbutcher1 <58036840+maxbutcher1@users.noreply.github.com>
2022-08-18 19:40:26 +0300

    Add files via upload

--- learn-english-with-basketball/index.js
+++ learn-english-with-basketball/index.js
@@ -1,64 +1,81 @@
 const categories = document.querySelector('.categories');
 const curr_cat = document.querySelector('.curr_cat');
-const genereteLetterButton = document.querySelector('#generete_letter_button');
-const resetCookieButton = document.querySelector('#reset_cookie_btn');
+const topButton = document.querySelector('.top');
+const backSymbol = document.querySelector('.back_to_main');
 
 
 //Кнопки категорій
 const cat1 = document.querySelector('#c1'); //random letter
 
 
-cat1.addEventListener('click',(e)=>{
+cat1.addEventListener('click', (e) => {
     curr_cat.style.display = 'flex';
     categories.style.display = 'none';
+    topButton.setAttribute("id", "backbtn")
+    backSymbol.style.display='block';
 });
 
 
-//#region 
+//#region Категорія "Генерація букв"
+const genereteLetterButton = document.querySelector('#generete_letter_button');
+const resetCookieButton = document.querySelector('#reset_cookie_btn');
 
-const charCookie  = document.cookie.match(new RegExp(
+const charCookie = document.cookie.match(new RegExp(
     "(?:^|; )" + "characters".replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-  ))
+))
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 let characters = charCookie ? decodeURIComponent(charCookie[1]) : letters;
 
-if(characters === ""){
+if (characters === "") {
     genereteLetterButton.style.display = "none";
     resetCookieButton.style.display = "block";
 }
-else{
+else {
     resetCookieButton.style.display = "none";
     genereteLetterButton.style.display = "block";
 }
 
-/*Кнопка генерації букв */
-genereteLetterButton.addEventListener('click',()=>{
+    /*Кнопка генерації букв */
+genereteLetterButton.addEventListener('click', () => {
 
     if (characters === "") {
         console.log("всі букви закінчились!!");
         resetCookieButton.style.display = "block";
         genereteLetterButton.style.display = "none";
     }
-    
-    let result = characters.charAt(Math.floor(Math.random()* characters.length));
+
+    let result = characters.charAt(Math.floor(Math.random() * characters.length));
     const resultTag = document.querySelector('.curr_cat p');
-    characters = characters.replace(result,"")
+    characters = characters.replace(result, "")
     resultTag.innerHTML = (result);
     document.cookie = "characters" + "=" + encodeURIComponent(characters);
 });
-//#endregion
-
-resetCookieButton.addEventListener('click',()=>{
+resetCookieButton.addEventListener('click', () => {
     console.log("reset");
     deleteCookie("characters");
     resetCookieButton.style.display = "none";
     genereteLetterButton.style.display = "block"
-    
-    
-});
 
-function deleteCookie(name){
-    document.cookie=name+'=; expires='+Date()+';'+'path=/;';
+
+});
+//#endregion
+
+topButton.addEventListener("click",()=>{
+    if(topButton.getAttribute("id")){
+        backToMain(curr_cat,categories);
+    }
+})
+
+function backToMain(closeElement, openElement){
+    closeElement.style.display = 'none';
+    openElement.style.display = 'flex';
+    backSymbol.style.display='none';
+}
+
+
+
+function deleteCookie(name) {
+    document.cookie = name + '=; expires=' + Date() + ';' + 'path=/;';
     characters = letters;
     console.log("deleteCookie");
 }
